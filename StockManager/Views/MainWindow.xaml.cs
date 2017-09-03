507a6d8104f332183ef61cdeb50a9c2e585effc5
Reenuay <Reenuay@users.noreply.github.com>
2017-09-03 17:56:03 +0400

    Переходим к паттерну MVVM

--- StockManager/Views/MainWindow.xaml.cs
+++ StockManager/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StockManager
+namespace StockManager.View
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
