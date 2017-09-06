9711a62f4a70d95b7b373b3faed72fe5c784c71c
Reenuay <Reenuay@users.noreply.github.com>
2017-09-06 22:22:50 +0400

    Создана базовая структура папок и базовый шаблон приложения.

---
+++ StockManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StockManager.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public MainMenuItem[] MainMenuItems
+        {
+            get
+            {
+                return new MainMenuItem[] {
+                    new MainMenuItem
+                    {
+                        Name = "Home",
+                        Icon = "Home"
+                    },
+                    new MainMenuItem
+                    {
+                        Name = "Icons",
+                        Icon = "FileTree"
+                    },
+                    new MainMenuItem
+                    {
+                        Name = "Sets",
+                        Icon = "PackageVariant"
+                    },
+                    new MainMenuItem
+                    {
+                        Name = "Keywords",
+                        Icon = "Key"
+                    }
+                };
+            }
+        }
+    }
+
+    struct MainMenuItem
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+    }
+}
--- StockManager/Views/MainWindow.xaml.cs
+++ StockManager/Views/MainWindow.xaml.cs
@@ -12,17 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using StockManager.ViewModels;
 
-namespace StockManager.View
+namespace StockManager.Views
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
